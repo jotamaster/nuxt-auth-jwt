eac03b7a0b7f1fef84dc356a87be3b9b30fa02a6
jotamaster <j3anhernandez@gmail.com>
2019-10-06 18:29:38 -0400

    add

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,34 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-auth-jwt
-      </h1>
-      <h2 class="subtitle">
-        My supreme Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div v-if="$store.state.auth">
+      <p>
+        You are authenticated. You can see the
+        <NuxtLink to="/secret">
+          secret page
+        </NuxtLink>!
+      </p>
+      <button @click="logout">
+        Logout
+      </button>
     </div>
+    <p v-else>
+      Please
+      <NuxtLink to="/login">
+        login
+      </NuxtLink>.
+    </p>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+const Cookie = process.client ? require('js-cookie') : undefined
 
 export default {
-  components: {
-    Logo
+  methods: {
+    logout () {
+      Cookie.remove('auth')
+      this.$store.commit('setAuth', null)
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
